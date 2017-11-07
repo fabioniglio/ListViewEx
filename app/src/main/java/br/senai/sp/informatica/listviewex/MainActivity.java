08c1275608d25b80b5ddb2ac4c6b7196a86b3d08
Fabio Niglio <fabioniglio@gmail.com>
2017-11-07 21:54:51 -0200

    ListView

--- app/src/main/java/br/senai/sp/informatica/listviewex/MainActivity.java
+++ app/src/main/java/br/senai/sp/informatica/listviewex/MainActivity.java
@@ -2,12 +2,23 @@ package br.senai.sp.informatica.listviewex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //recupera a listview do layout
+        listView = (ListView)findViewById(R.id.list_view);
+
+        // associa o listview
+
+        listView.setAdapter(new Adaptador(this));
+
     }
 }
